d74eff3ae1cce5d32bbfdba24b551065f8f6d63a
Brandon Dyer <BrandonDyer64@gmail.com>
2020-02-23 19:46:23 -0600

    #2 | Ping command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use serenity::client::Client;
+use serenity::framework::standard::{
+    macros::{command, group},
+    CommandResult, StandardFramework,
+};
+use serenity::model::channel::Message;
+use serenity::prelude::{Context, EventHandler};
+
+#[group]
+#[commands(ping)]
+struct General;
+
+use std::env;
+
+struct Handler;
+
+impl EventHandler for Handler {}
+
 fn main() {
-    println!("Hello, world!");
+    // Login with a bot token from the environment
+    let mut client = Client::new(&env::var("DISCORD_TOKEN").expect("token"), Handler)
+        .expect("Error creating client");
+
+    client.with_framework(
+        StandardFramework::new()
+            .configure(|c| c.prefix("-viav ")) // set the bot's prefix to "~"
+            .group(&GENERAL_GROUP),
+    );
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start() {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!")?;
+
+    Ok(())
 }
