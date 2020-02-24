00b07fbd389cb36807713996f06fc2ad3c94d4fa
Brandon Dyer <brandondyer64@gmail.com>
2020-02-24 14:04:29 -0600

    #6 | Voice channel automation

--- src/handler.rs
+++ src/handler.rs
@@ -22,20 +22,24 @@ impl EventHandler for Handler {
             None => return,
         };
 
-        let new_id_num = new.channel_id.unwrap_or(ChannelId(0)).0;
-        let old_id_num = match &old {
-            Some(old_id) => old_id.channel_id.unwrap_or(ChannelId(0)).0,
-            None => 0,
+        let new_id: ChannelId = new.channel_id.unwrap_or(ChannelId(0));
+        let old_id: ChannelId = match &old {
+            Some(old_id) => old_id.channel_id.unwrap_or(ChannelId(0)),
+            None => ChannelId(0),
         };
 
-        println!("{} : {}", new_id_num, old_id_num);
+        println!("{} : {}", new_id, old_id);
 
-        if new_id_num != old_id_num {
-            if old_id_num != 0 {
-                voice_events::on_leave(&mut ctx, guild_id, old);
+        if new_id != old_id {
+            if old_id != 0 {
+                if let Some(channel) = old_id.to_channel(&ctx).unwrap().guild() {
+                    voice_events::on_leave(&mut ctx, guild_id, channel);
+                }
             }
-            if new_id_num != 0 {
-                voice_events::on_join(&mut ctx, guild_id, new);
+            if new_id != 0 {
+                if let Some(channel) = new_id.to_channel(&ctx).unwrap().guild() {
+                    voice_events::on_join(&mut ctx, guild_id, channel);
+                }
             }
         }
     }
--- src/voice_events.rs
+++ src/voice_events.rs
@@ -1,15 +1,52 @@
-use serenity::{model::id::GuildId, model::voice::VoiceState, prelude::Context};
+use serenity::prelude::RwLock;
+use serenity::{
+    model::{
+        channel::{ChannelType, GuildChannel},
+        id::GuildId,
+    },
+    prelude::Context,
+};
+use std::sync::Arc;
 
-pub fn on_join(_ctx: &mut Context, _guild_id: GuildId, _voice_state: VoiceState) -> Option<()> {
+pub fn on_join(
+    ctx: &mut Context,
+    guild_id: GuildId,
+    voice_channel: Arc<RwLock<GuildChannel>>,
+) -> Option<()> {
     println!("JOIN");
+    if voice_channel.read().members(&ctx).ok()?.len() != 1 {
+        return None;
+    }
+    guild_id
+        .create_channel(ctx, |c| {
+            let mut create_channel = c
+                .kind(ChannelType::Voice)
+                .name(voice_channel.read().name.clone())
+                .position((voice_channel.read().position - 1) as u32)
+                .permissions(voice_channel.read().permission_overwrites.clone());
+
+            if let Some(category_id) = voice_channel.read().category_id {
+                create_channel = create_channel.category(category_id);
+            }
+
+            if let Some(user_limit) = voice_channel.read().user_limit {
+                create_channel = create_channel.user_limit(user_limit as u32);
+            }
+
+            create_channel
+        })
+        .ok()?;
     Some(())
 }
 
 pub fn on_leave(
-    _ctx: &mut Context,
+    ctx: &mut Context,
     _guild_id: GuildId,
-    _voice_state: Option<VoiceState>,
+    voice_channel: Arc<RwLock<GuildChannel>>,
 ) -> Option<()> {
     println!("LEAVE");
+    if voice_channel.read().members(&ctx).ok()?.len() == 0 {
+        voice_channel.read().delete(&ctx).ok()?;
+    }
     Some(())
 }
