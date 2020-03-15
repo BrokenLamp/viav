370f8d89aad29d13b80fee223635b1ad9dca3d10
Brandon Dyer <BrandonDyer64@gmail.com>
2020-03-15 18:32:31 -0500

    Text channel local visibility

--- src/voice_create.rs
+++ src/voice_create.rs
@@ -1,17 +1,14 @@
+use super::deck;
 use lazy_static::lazy_static;
 use serenity::model::channel::ChannelType;
 use serenity::model::channel::PermissionOverwrite;
 use serenity::model::channel::PermissionOverwriteType;
-use serenity::model::channel::ReactionType;
 use serenity::model::permissions::Permissions;
-use serenity::model::prelude::EmojiId;
 use serenity::model::prelude::GuildChannel;
 use serenity::model::prelude::GuildId;
+use serenity::model::prelude::RoleId;
 use serenity::model::prelude::UserId;
 use serenity::prelude::Context;
-use serenity::prelude::RwLock;
-use serenity::utils::Colour;
-use std::sync::Arc;
 use std::sync::Mutex;
 
 lazy_static! {
@@ -21,29 +18,10 @@ lazy_static! {
 pub fn voice_create(
     ctx: &Context,
     guild_id: GuildId,
-    voice_channel: Arc<RwLock<GuildChannel>>,
+    voice_channel: &GuildChannel,
     user_id: UserId,
 ) -> Option<()> {
-    let voice_channel = voice_channel.read();
-    let channel_type = ChannelType::Voice;
-    guild_id
-        .create_channel(ctx, |c| {
-            let mut create_channel = c
-                .kind(channel_type)
-                .name::<&str>(voice_channel.name.as_ref())
-                .permissions(voice_channel.permission_overwrites.clone());
-
-            if let Some(category_id) = voice_channel.category_id {
-                create_channel = create_channel.category(category_id);
-            }
-
-            if let Some(user_limit) = voice_channel.user_limit {
-                create_channel = create_channel.user_limit(user_limit as u32);
-            }
-
-            create_channel
-        })
-        .ok()?;
+    duplicate_voice_channel(ctx, guild_id, voice_channel)?;
 
     let id = {
         let mut lock = ID.lock().unwrap();
@@ -72,62 +50,67 @@ pub fn voice_create(
     );
 
     let channel_type = ChannelType::Text;
-    guild_id
+    let text_channel = guild_id
         .create_channel(ctx, |c| {
             let mut create_channel = c
                 .kind(channel_type)
-                .topic(format!("**Screen Share: {}** - &{}&{}", screen_share_link, voice_channel.id.0, user_id.0))
+                .topic(format!(
+                    "**Screen Share: {}** - &{}&{}",
+                    screen_share_link, voice_channel.id.0, user_id.0
+                ))
                 .name(format!("voice-viav-{}", id))
-                .permissions(voice_channel.permission_overwrites.clone());
+                .permissions(vec![PermissionOverwrite {
+                    allow: Permissions::empty(),
+                    deny: Permissions::READ_MESSAGES,
+                    kind: PermissionOverwriteType::Role(RoleId::from(guild_id.0)),
+                }]);
 
             if let Some(category_id) = voice_channel.category_id {
                 create_channel = create_channel.category(category_id);
             }
             create_channel
         })
-        .ok()?
-        .send_message(ctx, |m| {
-            m.embed(|e| {
-                e.author(|a| {
-                    a.name(new_name)
-                        .icon_url("https://cdn.discordapp.com/attachments/451092625894932493/681741191313883186/Viav.png")
-                        .url("https://viav.app/")
-                })
-                .field(
-                    "Video",
-                    format!("[` Share Screen `]({})", screen_share_link),
-                    true,
-                )
-                .field("Like Viav?", "[` Vote on Top.gg `](https://top.gg/bot/446151195338473485/vote)", true)
-                .field("Owner", format!("<@{}>", user_id.0), true)
-                .colour(Colour::from_rgb(103, 58, 183))
-            })
-            .reactions(vec![
-                ReactionType::Custom {
-                    animated: false,
-                    id: EmojiId(684471911920566281),
-                    name: Some(String::from("lock")),
-                },
-                // ReactionType::Custom {
-                //     animated: false,
-                //     id: EmojiId(684471928739725376),
-                //     name: Some(String::from("eye")),
-                // },
-                ReactionType::Custom {
-                    animated: false,
-                    id: EmojiId(684470685430448128),
-                    name: Some(String::from("alert")),
-                },
-                // ReactionType::Custom {
-                //     animated: false,
-                //     id: EmojiId(684471126130425935),
-                //     name: Some(String::from("help")),
-                // },
-            ])
-        })
-        .ok()?
+        .ok()?;
+
+    deck::create_deck(ctx, &text_channel, new_name, screen_share_link, user_id)?
         .pin(ctx)
         .ok()?;
 
+    text_channel
+        .create_permission(
+            ctx,
+            &PermissionOverwrite {
+                allow: Permissions::READ_MESSAGES,
+                deny: Permissions::empty(),
+                kind: PermissionOverwriteType::Member(user_id),
+            },
+        )
+        .ok()?;
+
     Some(())
+}
+
+fn duplicate_voice_channel(
+    ctx: &Context,
+    guild_id: GuildId,
+    voice_channel: &GuildChannel,
+) -> Option<GuildChannel> {
+    guild_id
+        .create_channel(ctx, |c| {
+            let mut create_channel = c
+                .kind(ChannelType::Voice)
+                .name::<&str>(voice_channel.name.as_ref())
+                .permissions(voice_channel.permission_overwrites.clone());
+
+            if let Some(category_id) = voice_channel.category_id {
+                create_channel = create_channel.category(category_id);
+            }
+
+            if let Some(user_limit) = voice_channel.user_limit {
+                create_channel = create_channel.user_limit(user_limit as u32);
+            }
+
+            create_channel
+        })
+        .ok()
 }
